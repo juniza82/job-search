cacbfc15172f9801870642313e11cb95fa16dfa9
fbwotjq <fbwotjq@gmail.com>
2017-11-18 18:56:46 +0900

    modify source

--- src/main/java/egovframework/search/common/WNCollection.java
+++ src/main/java/egovframework/search/common/WNCollection.java
@@ -106,7 +106,7 @@ public class WNCollection {
 				"RANK/DESC,DATE/DESC",  // set sort field (field,order) multi sort '/'
 				"basic,rpfmo,100",  // set sort field (field,order) multi sort '/'
 				"TITLE/100,CONTENT/30",// set search field
-				"DOCID,BOARD_ID,TITLE,CONTENT,DATE,ALIAS",// set document field
+				"DOCID,BD_ID,BOARD_ID,BOARD_NM,TITLE,CONTENT/200,RDATE,WRITER,ALIAS",// set document field
 				"", // set date range
 				"", // set rank range
 				"", // set prefix query, example: <fieldname:contains:value1>|<fieldname:contains:value2>/1,  (fieldname:contains:value) and ' ', or '|', not '!' / operator (AND:1, OR:0)
